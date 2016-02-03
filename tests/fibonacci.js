acfed27910fecf9c1401c6b4a0ede8cc58f3851d
Baz <bk@thinkloop.com>
2016-02-03 22:40:00 +0000

    added tests and refactored

--- tests/fibonacci.js
+++ tests/fibonacci.js
@@ -39,37 +39,3 @@ describe("fibonacci", () => {
     it("should have proper lru length", () => { expect(fibonacciMemoized.lru.length).toEqual(41); });
     it("should have significantly higher performance", () => { expect(fibonacciTime[0] - fibonacciMemoizedTime[0] >= 2).toEqual(true); });
 });
-
-describe("complex args", () => {
-    var memoizedFn,
-        arg1 = { a: { b: 3 }, num: 3 },
-        arg2 = { c: { d: 3 }, num: 7 },
-        arg3 = [{ f: { g: 3 }, num: 11 }, { h: { i: 3 }, num: 4 }, { j: { k: 3 }, num: 6 }];
-
-    beforeEach(function() {
-        memoizedFn = Memoizerific(50)(function(arg1, arg2, arg3) {
-            return arg1.num * arg2.num;
-        });
-        memoizedFn(arg1, arg2, arg3);
-    });
-
-    it("should be map or similar", () => { expect(memoizedFn.cache instanceof Map).toEqual(process.env.TEST_MAPORSIMILAR !== 'true'); });
-
-    it("should not be memoized", () => {
-        expect(memoizedFn.wasMemoized).toEqual(false);
-        expect(memoizedFn.lru.length).toEqual(1);
-    });
-
-    it("should be memoized", () => {
-        memoizedFn(arg1, arg2, arg3);
-        expect(memoizedFn.wasMemoized).toEqual(true);
-        expect(memoizedFn.lru.length).toEqual(1);
-    });
-
-    it("should have multiple cached items", () => {
-        memoizedFn(arg1, arg2, arg3);
-        memoizedFn(arg1, arg2, 1);
-        expect(memoizedFn.wasMemoized).toEqual(false);
-        expect(memoizedFn.lru.length).toEqual(2);
-    });
-});
